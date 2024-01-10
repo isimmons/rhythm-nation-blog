5a63912bd4f9fc9ca50ed4c52ad94028af646c16
isimmons <isimmons33@gmail.com>
2024-01-09 22:47:35 -0500

    ah it causes the failed to fetch dynamic blah blah useless crap message

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-// src/middleware.js
-import { sequence } from "astro:middleware";
-import * as Sentry from "@sentry/astro";
-
-export const onRequest = sequence(
-  Sentry.handleRequest()
-  // Add your other handlers after Sentry.handleRequest()
-);
