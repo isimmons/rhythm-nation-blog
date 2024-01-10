90a6893e427da69f2df6c4b02bbda941f2ef2a44
isimmons <isimmons33@gmail.com>
2024-01-09 19:31:30 -0500

    another oopsie

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), icon(), svelte()],
   site: "https://rythm-nation-blog.vercel.app",
   output: "server",
   adapter: vercel({
