a9984b440f9ca8de4c1c828d8e555af55f553888
isimmons <isimmons33@gmail.com>
2024-01-09 22:11:34 -0500

    see if this site map builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless";
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 import sentry from "@sentry/astro";
 
 // https://astro.build/config
@@ -11,6 +12,7 @@ export default defineConfig({
     tailwind(),
     icon(),
     svelte(),
+    sitemap(),
     sentry({
       dsn: "https://6b3d9cf217f575ee9ca356327415e129@o1078821.ingest.sentry.io/4506544480124928",
       sourceMapsUploadOptions: {
