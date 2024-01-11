609b0f1f7a0942c9d32a8a17c89624861f9bcf7e
isimmons <isimmons33@gmail.com>
2024-01-11 16:31:17 -0500

    make devOnlyIntegrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,24 +7,27 @@ import sitemap from "@astrojs/sitemap";
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
+const devOnlyIntegrations = [
+  sentry({
+    dsn: "https://6b3d9cf217f575ee9ca356327415e129@o1078821.ingest.sentry.io/4506544480124928",
+    sourceMapsUploadOptions: {
+      project: "rhythm-nation-blog",
+      authToken: import.meta.env.SENTRY_AUTH_TOKEN,
+      telemetry: false,
+    },
+  }),
+  ,
+  spotlightjs(),
+];
+
+const integrations = [tailwind(), icon(), svelte(), sitemap()];
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    icon(),
-    svelte(),
-    sitemap(),
-    sentry({
-      dsn: "https://6b3d9cf217f575ee9ca356327415e129@o1078821.ingest.sentry.io/4506544480124928",
-      sourceMapsUploadOptions: {
-        project: "rhythm-nation-blog",
-        authToken: import.meta.env.SENTRY_AUTH_TOKEN,
-        telemetry: false,
-      },
-    }),
-    ,
-    spotlightjs(),
-  ],
+  integrations:
+    import.meta.env.MODE === "production"
+      ? integrations
+      : [...integrations, ...devOnlyIntegrations],
   site: "https://rhythm-nation-blog.vercel.app",
   output: "server",
   adapter: vercelServerless({
